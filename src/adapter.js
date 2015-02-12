b9ccff8764c90447f517c9ef5a6260ff2f70725b
s0ph1e <sonya1912@gmail.com>
2015-02-12 16:48:44 +0200

    Adapt result: transform first char to lower case

--- src/adapter.js
+++ src/adapter.js
@@ -1,26 +1,38 @@
 var _ = require('lodash');
 
-function capitalizeFirstChar (str) {
+function firstCharToUpperCase (str) {
 	return str.substr(0, 1).toUpperCase() + str.substr(1);
 }
 
-function adaptParams (params) {
-	if (_.isEmpty(_.keys(params))) {
-		return params;
+function firstCharToLowerCase (str) {
+	return str.substr(0, 1).toLowerCase() + str.substr(1);
+}
+
+function transformObject (obj, func) {
+	if (_.isEmpty(_.keys(obj))) {
+		return obj;
 	}
 
-	var adaptedParams = _.isArray(params) ? [] : {};
-	_.forOwn(params, function (value, key) {
-		var adaptedKey = capitalizeFirstChar(key);
-		adaptedParams[adaptedKey] = adaptParams(value);
+	var transformedObj = _.isArray(obj) ? [] : {};
+	_.forOwn(obj, function (value, key) {
+		var transformedKey = func(key);
+		transformedObj[transformedKey] = transformObject(value, func);
 	});
-	return adaptedParams;
+	return transformedObj;
+}
+
+function adaptParams (params) {
+	return transformObject(params, firstCharToUpperCase);
+}
+
+function adaptResult (res) {
+	return transformObject(res, firstCharToLowerCase);
 }
 
 function adaptMethod (method) {
 	return function adaptedMethod (params) {
 		var adaptedParams = adaptParams(params);
-		return method.call(this, adaptedParams);
+		return method.call(this, adaptedParams).then(adaptResult);
 	};
 }
 
